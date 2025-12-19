ebb3b7c7dfb59cd183a9af5ee4a68149e4cb2603
Lunexa-Project <chrispathoumphanh@gmail.com>
2025-12-19 20:11:26 +0800

    Added student information output
    Added student information output

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,6 @@ public class Main {
         System.out.println("BIT504 Task 9");
         System.out.println("Source and Version Control Demo");
         System.out.println("First feature branch update");
+        System.out.println("Student: Cristophe");
     }
 }
